d7e715ef7a48020e5f81ba9bf7a4ffd17a2ec5b6
Vishal Chudasama <vishal@padhyasoft.com>
2025-11-25 14:18:28 +0530

    Test Database connect or not

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using MatchMaking.Controllers;
+using MatchMaking.Infra;
+using MatchMaking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchMaking.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class HomeController : BaseController<dynamic>
+	{
+		public HomeController(IRepositoryWrapper repository) : base(repository) { }
+
+		public IActionResult Index()
+		{
+			var list = _context.Using<User>().GetAll().ToList();
+
+			return View();
+		}
+	}
+}
